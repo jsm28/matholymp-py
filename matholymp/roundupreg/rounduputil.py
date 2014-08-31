89f6f748b3b52d1153ed7d41c25b2c70860f0a5e
Joseph Myers <jsm@polyomino.org.uk>
2014-08-31 14:52:28 +0000

    Add various utility functions for use from registration system.
    Mostly taken direct from registration system code, but with some
    refactoring.

--- matholymp/roundupreg/rounduputil.py
+++ matholymp/roundupreg/rounduputil.py
@@ -32,9 +32,15 @@ This module provides various utility functions for the Roundup
 registration system.
 """
 
+import re
+
 from roundup.date import Date
 
-__all__ = ['scores_from_str', 'contestant_age']
+__all__ = ['scores_from_str', 'contestant_age', 'get_none_country',
+           'get_staff_country', 'normal_country_person',
+           'person_is_contestant', 'contestant_code', 'pn_score',
+           'scores_final', 'any_scores_missing', 'country_has_contestants',
+           'valid_country_problem']
 
 def scores_from_str(db, score_str):
     """
@@ -65,3 +71,80 @@ def contestant_age(db, person):
                                    date2t[2] < date1t[2]):
         diff -= 1
     return diff
+
+def get_none_country(db):
+    """Return the id of the special 'None' country."""
+    return db.country.lookup('None')
+
+def get_staff_country(db):
+    """Return the id of the special staff country."""
+    short_name = db.config.ext['MATHOLYMP_SHORT_NAME']
+    year = db.config.ext['MATHOLYMP_YEAR']
+    staff_country_name = short_name + ' ' + year + ' Staff'
+    return db.country.lookup(staff_country_name)
+
+def normal_country_person(db, userid):
+    """Determine whether the user is from a normal country."""
+    user_country = db.user.get(userid, 'country')
+    return (user_country != get_none_country(db) and
+            user_country != get_staff_country(db))
+
+def person_is_contestant(db, person):
+    """Determine whether a person is a contestant."""
+    role = db.person.get(person, 'primary_role')
+    return db.matholymprole.get(role, 'name').startswith('Contestant ')
+
+def contestant_code(db, person):
+    """Determine a contestant's contestant code."""
+    role = db.person.get(person, 'primary_role')
+    country = db.person.get(person, 'country')
+    number = db.matholymprole.get(role, 'name')[len('Contestant '):]
+    return db.country.get(country, 'code') + number
+
+def pn_score(db, person, n):
+    """Determine the score of a contestant on a given problem."""
+    score_str = db.person.get(person, 'scores')
+    scores = scores_from_str(db, score_str)
+    return scores[n-1]
+
+def scores_final(db):
+    """Determine whether the scores are final."""
+    gold = db.event.get('1', 'gold')
+    if gold is None or gold == '':
+        return False
+    else:
+        return True
+
+def any_scores_missing(db):
+    """Determine whether any scores have yet to be entered."""
+    person_list = db.person.list()
+    num_problems = int(db.config.ext['MATHOLYMP_NUM_PROBLEMS'])
+    for person in person_list:
+        if person_is_contestant(db, person):
+            score_str = db.person.get(person, 'scores')
+            scores = scores_from_str(db, score_str)
+            for i in range(num_problems):
+                if scores[i] == '':
+                    return True
+    return False
+
+def country_has_contestants(db, country):
+    """Determine whether a country has any contestants registered."""
+    people = db.person.filter(None, {'country': country})
+    for person in people:
+        if person_is_contestant(db, person):
+            return True
+    return False
+
+def valid_country_problem(db, form):
+    """Determine whether the country and problem are valid to enter scores."""
+    if 'country' not in form or 'problem' not in form:
+        return False
+    num_problems = int(db.config.ext['MATHOLYMP_NUM_PROBLEMS'])
+    country = form['country'].value
+    problem = form['problem'].value
+    if not re.match('^[1-9][0-9]*\\Z', problem):
+        return False
+    if int(problem) > num_problems:
+        return False
+    return country_has_contestants(db, country)
