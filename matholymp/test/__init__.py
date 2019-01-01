3e46a3e48c7443f1db11f70218172ea8654308d7
Joseph Myers <jsm@polyomino.org.uk>
2019-01-01 12:14:06 +0000

    Update copyrights for 2019 and bump version to 2019.01.0.dev0.

--- matholymp/test/__init__.py
+++ matholymp/test/__init__.py
@@ -1,6 +1,6 @@
 # Initialise matholymp.test subpackage.
 
-# Copyright 2014-2018 Joseph Samuel Myers.
+# Copyright 2014-2019 Joseph Samuel Myers.
 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
