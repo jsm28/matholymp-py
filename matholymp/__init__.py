f916a649286ac6dc43cf293753add71f07afa15b
Joseph Myers <jsm@polyomino.org.uk>
2019-03-17 20:01:54 +0000

    Update version number to 2019.03.0.dev0.

--- matholymp/__init__.py
+++ matholymp/__init__.py
@@ -33,7 +33,7 @@ maintaining websites for mathematical olympiads, registration of
 participants and associated administration.
 """
 
-__version__ = '2019.02.0.dev0'
+__version__ = '2019.03.0.dev0'
 
 __all__ = ['caseconv', 'collate', 'csvsource', 'data', 'datasource',
            'datetimeutil', 'docgen', 'fileutil', 'regdata', 'regsitegen',
