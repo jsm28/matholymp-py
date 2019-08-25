c25c9d3eb101b6eb02fcca62108f1a2de2765710
Joseph Myers <jsm@polyomino.org.uk>
2019-08-25 23:42:57 +0000

    Add EditCountry permission.
    This controls which parts of country.item.html get shown.  This is
    preparation for giving registering users the permission to edit, for
    their own country, details of the expected numbers of participants in
    different roles and the expected number of single room requests (via a
    separate page template); giving this limited ability to edit the
    country class should not thereby result in various additional edit
    interfaces and associated text appearing when the default country
    template is viewed.

--- matholymp/roundupreg/schema.py
+++ matholymp/roundupreg/schema.py
@@ -444,6 +444,13 @@ def init_schema(env):
     p = db.security.addPermission(name='RegisterPhone')
     db.security.addPermissionToRole('Admin', p)
 
+    # Permission to edit countries in general, rather than just a
+    # limited subset of properties.  This is only used to control
+    # whether the interface is shown to edit various fields, not for
+    # actual checks of whether a given edit is permitted.
+    p = db.security.addPermission(name='EditCountry')
+    db.security.addPermissionToRole('Admin', p)
+
     # Viewing registration status pages (whether for everyone, or just
     # for one's own country) has its own permission.
     p = db.security.addPermission(name='RegistrationStatus')
